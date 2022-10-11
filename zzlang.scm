85aa30eec73f6502d33ac4b82bfd22c771b0bb41
Will Dey <52264243+wi11dey@users.noreply.github.com>
2022-10-11 15:29:55 -0400

    eval-less experiment

--- zzlang.scm
+++ zzlang.scm
@@ -4,96 +4,10 @@
 
 ;;; zzlang.scm --- R5RS-compliant interpreter for a subset of zlang for bootstrapping purposes.
 ;; Do NOT use for any other purpose. zzlang is unoptimized, outputs only binary, may silently ignore some invalid inputs, does not guarantee termination, and has limited error-handling capabilities.
-;; Additionally, zzlang uses some Scheme tricks to simplify implementation at the cost of performance, which is not a concern for bootstrapping.
 
-;; TODO learn higher category theory and H-M type system with overloading, and make zlang <-> text system before continuing implementation
-
-;;; Utilities:
-
-(define-syntax define-tail-cons
-  ;; Transforms tail-recursive-modulo-cons functions into properly tail-recursive ones.
-  (syntax-rules (lambda)
-    ((define-tail-cons (name . args) . body)
-     (define (name . args)
-       (letrec ((f (lambda (head last . args)
-		     (let* ((recurse #f)
-			    (name (lambda new-args
-				    (set! recurse new-args)
-				    '()))
-			    (return (begin . body)))
-		       (set-cdr! last return)
-		       (if recurse
-			   (apply f head return recurse)
-			   head))))
-		(head (cons '() '())))
-	 (cdr (f head head . args)))))
-    ((define-tail-cons  name (lambda args . body))
-     (define-tail-cons (name . args) . body))))
-
-(define (string-replace s char1 char2)
-  (set! s (string-copy s))
-  (do ((i 0 (+ i 1)))
-      ((= i (string-length s)) s)
-    (if (char=? (string-ref s i) char1)
-	(string-set! s i char2))))
-
-(define (string-split separator s)
-  (define-tail-cons (splitter start end length)
-    (if (< end length)
-	(if (char=? (string-ref s end) separator)
-	    (cons (substring s start end)
-		  (splitter s (+ end 1) (+ end 1) length))
-	    (splitter s start (+ end 1) length))
-	(list (substring (if (< start length)
-			     start length)
-			 length))))
-  (splitter 0 0 (string-length s)))
-
-(define (string-join separator . strings)
-  (if (null? strings)
-      ""
-      (apply string-append
-	     (car strings)
-	     (let ((string-separator (string separator)))
-	       (map (lambda (s)
-		      (string-append separator s))
-		    (cdr strings))))))
-
-(define-tail-cons (read-forms file)
-  (let ((form (read file)))
-    (if (eof-object? form)
-	'()
-	(cons form (read-forms file)))))
-
-(define-syntax push
-  (syntax-rules ()
-    ((push newelt place)
-     (set! place (cons newelt place)))))
-
-;; Uses `eval' to use syntax-rules mechanism to pattern-match data known only at runtime.
-(define-syntax define-match
-  (syntax-rules (quote)
-    ((define-match name
-       '(literal ...)
-       (pattern . then) ...)
-     (define (name x)
-       (eval
-	`(letrec-syntax
-	     ((name ; Slight optimization to remove recursive `eval'.
-	       (syntax-rules (quote)
-		 ((name 'y)
-		  (matcher . y))))
-	      (matcher
-	       (syntax-rules (literal ...)
-		 ((matcher . pattern)
-		  (begin . then)) ...)))
-	   (matcher . ,x))
-	(interaction-environment))))
-    ((define-match name . rules)
-     (define-match name '() . rules))))
-
-
-;;; Implementation:
+(define environment '())
+(define invocations 0)
+(define )
 
 (define error #f)
 (let ((error-message
@@ -104,12 +18,13 @@
 	  #f))))
   (if error-message
       (display "error: ")
-      (for-each (lambda (part)
-		  (display part))
-		error-message)
+      (for-each
+       (lambda (part)
+	 (display part))
+       error-message)
       (newline)))
 
-(define (zz-print list)
+(define (dump list)
   (for-each
    (lambda (element)
      (let ((hex (number->string element 16)))
@@ -124,101 +39,67 @@
 	    16))))))
    list))
 
-;; (define (zz-intern identifier)
-;;   (string->symbol (string-append "-zz-" (symbol->string identifier))))
+(define (qualify context symbol)
+  (string->symbol (string-append (symbol->string context) "." (symbol->string symbol))))
 
-;; (define (zz-define identifier . definition)
-;;   (eval `(define ,(zz-intern identifier)
-;; 	   ,@definition)
-;; 	(interaction-environment)))
+(define (match? type candidate)
+  (let compare ((type-chars      (string->list (symbol->string type)))
+		(candidate-chars (string->list (symbol->string candidate))))
+    (if (null? type-chars)
+	(or (null? candidate-chars)
+	    (char=? (car candidate-chars) #\.))
+	(and (not (null? candidate-chars))
+	     (char=? (car type-chars)
+		     (car candidate-chars))
+	     (compare (cdr type-chars)
+		      (cdr candidate-chars))))))
 
-;; (define (zz-lookup identifier)
-;;   (eval `,(zz-intern identifier)
-;; 	(interaction-environment)))
+(define (lower))
 
-(define zz-definitions
-  '((var1 . #(definition))
-    (var2 . #(5))
-    (func1 . ((`((argtype ,arg) #f) . body)
-	      (`(#t (argtype ,arg)) . body)))))
-(define zz-wildcard
-  '((`((argtype ,arg) #f) . body)
-    (`(#t (argtype ,arg)) . body)))
+(define (def environment context signature . body)
+  (define (parameter form)
+    )
+  (cond ((symbol? signature)
+	 (let ((result (apply evaluate
+			      environment
+			      (qualify context signature)
+			      body))))
+	 (cons
+	  (car result))
+	 )
+	((list? signature)
+	 (if (not (symbol? (car signature)))))
+	(t
+	 (error signature " is not a valid identifier"))))
 
-(define (zz-insert new definitions)
-  (define-match zz-<
-    '(function quote)
-    (((function args1 _) . (function args2 . _))
-     )
-    (((function . _) . nonfunction)
-     #f)
-    ((nonfunction       . (function . _))
-     #t)
-    (('()))
-    ((a               . b)
-     (error "cannot redefine")))
-  (if (null? definitions)
-      (list new)
-      (do ((head definitions (cdr head)))
-	  ((zz-< head))))
-  )
+(define (normalize define ))
 
-(define-match zz-eval
-  '(define quote quasiquote unquote function apply)
-  ((define '(f . args) . body) (zz-eval '(define 'f (function 'args . body))))
-  ((define `(f . args) . body) (zz-eval '(define `f (function `args . body))))
-  ((define `,var value)
-   (push (zz-eval 'value) zz-wildcard)
-   ;; Wildcard definition.
-   ;; Only one possible value in any given scope for the wildcard variable, but can be many for functions depending on argument categories.
-   )
-  ((define `var value) (zz-eval '(define 'var value)))
-  ((define 'var value)
-   (if (not (symbol? var))
-       (error "cannot define non-symbol " var))
-   (push (cons var (zz-eval 'value)) zz-definitions)
-   ;; If the value is a function:
-   
-   ;; If value is not a function, then the following works:
-   (zz-define var (zz-eval 'value)))
-  ((function args . body)
-   '(function args . body))
-  ('quoted
-   'quoted)
-  ((apply f . args)
-   (let ((definition (assq 'f zz-definitions)))
-     (if definition
-
-	 (error 'f " undeclared"))))
-  ((f . args)
-   (zz-eval '(apply f . args)))
-  (constant
-   'constant))
-
-(define (zz-repl port print)
-  (do ((form   ''()       (read))
-       (result  '()       (zz-eval form)))
-      ((eof-object? form) (print result))))
+(define (evaluate context . forms)
+  (define (relabel form)
+    )
+  (cons context
+	(cond ((null? forms) forms)
+	      ((and (list? (car forms))
+		    (not (null? (car forms)))
+		    (eq? (caar forms) 'define))
+	       (apply evaluate
+		      (apply def environment context
+			     (cdar forms))
+		      context
+		      (cdr forms)))
+	      (#t
+	       (car forms)))))
 
 (define (main . files)
-  ;; Bootstrap file:
-  (call-with-input-file (car files)
-    (lambda (file) (zz-repl file (lambda _ _))))
-  ;; Library files:
-  (let ((filename->module
-	 (lambda (filename)
-	   (map string->symbol
-		(string-split #\/ (substring
-				   filename
-				   0 (- (string-length filename) 3)) ; Remove .zl extension.
-			      )))))
-    (for-each
-     (lambda (filename)
-       (zz-eval `(file
-		  (module ,@(file->module))
-		  ,@(call-with-input-file filename read-forms))))
-     (cdr files)))
-  ;; Executable file:
-  (zz-repl (current-input-port) zz-print))
+  (define (load port)
+    (let ((form (read port)))
+      (if (eof-object? form)
+	  '()
+	  (cons form (load port)))))
+  (dump
+   (evaluate '() #f
+	     (apply append (map (lambda (file)
+				  (call-with-input-file file load))
+				files)))))
 
 ;;; zzlang.scm ends here
