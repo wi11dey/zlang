577a50408401c1f5f845ff908f0a3c6d1df7028f
Will Dey <wdey@palantir.com>
2023-07-19 23:25:12 -0400

    WIP

--- haskell/Zlang/Monad.hs
+++ haskell/Zlang/Monad.hs
@@ -8,7 +8,68 @@ import Zlang.Expression
 -- - Floating-point literals: 0.5 is syntactic sugar for (rational 5 10)
 -- - Complex literals 1/2+0.5i is syntactic sugar for (complex ((real part) (rational 1 2)) ((imaginary part) (rational 5 10)))
 
+data SExpression sym  = Symbol sym
+                         | Value val
+                         | Pair SExpression SExpression
+                         deriving Eq
+
+instance (Show sym, Show val) => Show (SExpression sym val) where
+  show (Symbol s) = show s
+  show (Value  v) = show v
+  showPrec (Pair car cdr) = a
+
+
+
+data LispValue = Boolean Bool
+               | Character Char
+               | Vector [LispExpression]
+               | Integer Integer
+               | Rational Integer Integer
+               | Real Double
+               | Complex (Complex Double)
+               | String String
+               | Empty
+               deriving Eq
+
+instance Show LispValue where
+  show (Boolean True ) = "#t"
+  show (Boolean False) = "#f"
+  show (Character c) = "#\\" ++ c
+  show (Vector v) = do
+    "#("
+    element <- vec
+    show element
+    ")"
+
+type LispExpression = SExpression String LispValue
+
+
+
+type Object = SExpression String Function
+
 type Closure = Environment Value
+
+data Binding = Exact String
+             | Any   String
+
+data Pattern = SExpression (Maybe Binding) (Maybe Binding)
+
+data Function = Function (Maybe Pattern) (Value -> Closure)
+
+data Value = Expression (SExpression String Value)
+           | Function (Maybe Pattern) (Value -> Closure)
+
+data Pattern = SExpression String
+
+data Value = Atom String
+           | Application Value Value
+           | Function (Maybe Pattern) (Value -> Closure)
+
+data Pattern = Exactly String
+             | Anything String
+
+dispatch :: Value -> Value -> Closure
+apply ()
 
 data Value = Symbol String
            | Application Value Value
@@ -17,6 +78,7 @@ data Value = Symbol String
 
 apply :: Value -> Value -> Environment Value
 apply (Symbol sym) (Closure cl) = do
+  -- Ask closure to look up a function sym with 
   return
 apply (Symbol sym) (Closure cl) = do
   return
