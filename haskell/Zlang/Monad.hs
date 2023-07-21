5cbde7e1b6abd48bf8b25e580f92c47e0d5b75b0
Will Dey <wdey@palantir.com>
2023-07-20 22:22:02 -0400

    WIP

--- haskell/Zlang/Monad.hs
+++ haskell/Zlang/Monad.hs
@@ -8,20 +8,20 @@ import Zlang.Expression
 -- - Floating-point literals: 0.5 is syntactic sugar for (rational 5 10)
 -- - Complex literals 1/2+0.5i is syntactic sugar for (complex ((real part) (rational 1 2)) ((imaginary part) (rational 5 10)))
 
-data SExpression = Symbol String
-                 | Pair SExpression SExpression
-                 | Boolean Bool
-                 | Character Char
-                 | Vector [LispExpression]
-                 | Integer Integer
-                 | Rational Integer Integer
-                 | Real Double
-                 | Complex (Complex Double)
-                 | String String
-                 | Empty
-                 deriving Eq
+data Expression = Symbol String
+                | Pair Expression Expression
+                | Boolean Bool
+                | Character Char
+                | Vector [Expression]
+                | Integer Integer
+                | Rational Integer Integer
+                | Real Double
+                | Complex (Complex Double)
+                | String String
+                | Empty
+                deriving Eq
 
-instance Show Lisp where
+instance Show Expression where
   show (Symbol s) = show s
   showPrec (Pair car cdr) = show
   show (Boolean True ) = "#t"
@@ -39,22 +39,39 @@ instance Show Lisp where
   show (String str) = "\"" ++ str ++ "\""
   show Empty = "()"
 
-
+instance Read Expression where
+  read = read
 
-type Value = Atom String
+
+(Value -> Closure)
+data Value = Atom String
            | Application Value Value
-           | Function (Maybe Pattern) (Value -> Closure)
+           | Function (Maybe Pattern) Closure
+
+newtype Local = Local (Maybe String)
+
+instance Eq Local where
+  Local Nothing == _ = False
+  _ == Local Nothing = False
+  Local (Just a) == Local (Just b) = a == b
+
+data Binding = Exactly String
+             | Anything Local
+
+data Pattern = Binding Binding
+             | Destructuring Binding Local
 
 type Closure = Environment Value
 
-data Binding = Match String
-             | Any   String
+data Definition = Definition Binding Value
 
-data Pattern = Bind (Maybe Binding)
-             | DestructuringBind (Maybe Binding) (Maybe String)
+define :: [Definition] -> Closure ()
+
+newtype SyntaxError = SyntaxError String
+
+parse :: Expression -> Value
 
 dispatch :: Value -> Value -> Closure
-apply ()
 
 data Value = Symbol String
            | Application Value Value
