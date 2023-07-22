63137b7547e0a272cfcbcf64f5e70672a0cb619d
Will Dey <wdey@palantir.com>
2023-07-22 11:44:45 -0400

    WIP

--- haskell/Zlang/Monad.hs
+++ haskell/Zlang/Monad.hs
@@ -1,5 +1,5 @@
 module Zlang.Monad where
-import Zlang.Expression
+import Zlang.SExpression
 
 -- Syntactic sugar for literals is desugared here:
 -- - Character literals: #\A is syntactic sugar for (character 65)
@@ -8,11 +8,11 @@ import Zlang.Expression
 -- - Floating-point literals: 0.5 is syntactic sugar for (rational 5 10)
 -- - Complex literals 1/2+0.5i is syntactic sugar for (complex ((real part) (rational 1 2)) ((imaginary part) (rational 5 10)))
 
-data Expression = Symbol String
-                | Pair Expression Expression
+data SExpression = Symbol String
+                | Pair SExpression SExpression
                 | Boolean Bool
                 | Character Char
-                | Vector [Expression]
+                | Vector [SExpression]
                 | Integer Integer
                 | Rational Integer Integer
                 | Real Double
@@ -21,7 +21,7 @@ data Expression = Symbol String
                 | Empty
                 deriving Eq
 
-instance Show Expression where
+instance Show SExpression where
   show (Symbol s) = show s
   showPrec (Pair car cdr) = show
   show (Boolean True ) = "#t"
@@ -39,13 +39,13 @@ instance Show Expression where
   show (String str) = "\"" ++ str ++ "\""
   show Empty = "()"
 
-instance Read Expression where
+instance Read SExpression where
   read = read
 
 
 data Value = Atom String
            | Application Value Value
-           | Function (Maybe Pattern) Closure
+           | Function Pattern Closure
 
 newtype Local = Local (Maybe String)
 
@@ -55,7 +55,7 @@ instance Eq Local where
   Local (Just a) == Local (Just b) = a == b
 
 data Binding = Exactly String
-             | Anything Local
+             | Any Local
 
 data Pattern = Binding Binding
              | Destructuring Binding Local
@@ -68,7 +68,30 @@ define :: [Definition] -> Closure ()
 
 newtype SyntaxError = SyntaxError String
 
-parse :: Expression -> Value
+toList :: SExpression -> Either SyntaxError [SExpression]
+toList Empty = return []
+toList pair@(Pair car cdr) =
+  case toList cdr of
+    Right tail -> car:tail
+    Left _ -> Left $ SyntaxError "Not a proper list: " ++ show pair
+toList sexp = Left $ SyntaxError "Expected list, got: " ++ show sexp
+
+toPattern :: SExpression -> Either SyntaxError Pattern
+toPattern (Symbol "_") = return $ Binding $ Any $ Local $ Nothing
+toPattern (Symbol s  ) = return $ Binding $ Exactly $ s
+toPattern (Pair (Symbol "quote") (Pair (Symbol s) ))
+
+desugar :: SExpression -> SExpression
+desugar (Boolean True) = Symbol "true"
+desugar (Boolean False) = Symbol "false"
+desugar (Character c) = Pair (Symbol "character") $ desugar $ Integer $ toEnum c
+
+fromSExpression :: SExpression -> Either SyntaxError Value
+fromSExpression (Symbol s) = return $ Atom s
+fromSExpression (Pair (Symbol "function") (Pair patt body)) = do
+  forms <- toList body
+fromSExpression invalid =
+  Left $ SyntaxError "Invalid syntax: " ++ show invalid
 
 dispatch :: Value -> Value -> Closure
 
