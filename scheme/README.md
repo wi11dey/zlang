e749cd05f97cce7a00d18f19148c9a6f423f2c9c
Will Dey <wdey@palantir.com>
2026-07-28 18:19:36 +0100

    more

--- scheme/README.md
+++
@@ -1,1 +0,0 @@
-I think this should probably be a Lisp-2, in the sense that `funcall` is necessary to trigger multiple dispatch.
--- zlang.pl
+++ zlang.pl
@@ -5,7 +5,12 @@ sexp([quasiquote, X]) --> "`", !, sexp(X).
 sexp([unquote,    X]) --> ",", !, sexp(X).
 
 sexp(S) --> "(", !, sexps(S), ")".
+sexp(N) --> numeric(N).
 sexp(S) --> symbol(S).
+
+numeric(N) -->
+    string_without(`() \t\n\r`, [C|Cs]),
+    { catch(number_codes(N, [C|Cs]), _, fail) }.
 
 symbol(S) -->
     string_without(`() \t\n\r`, [C|Cs]),
@@ -16,25 +21,27 @@ sexps([])     --> blanks.
 
 zread(String, Sexp) :- string_codes(String, Codes), phrase(sexp(Sexp), Codes).
 
-zdefine([define, [quote, Name], Body]) :- repl.
-zdefine([define, [quasiquote, Name], Body]) :- repl.
+:- dynamic expand/2.
+
+zdefine([define, [quote, Name], Body]) :-
+    assertz(expand(Name, Body)).
+zdefine([define, [quasiquote, Name], Body]) :-
+    writeln('expand(', Name, ', Result).').
 zdefine([define, Name|_]) :-
     !,
     zread(String, Name),
     writeln('Cannot define', String),
     fail.
 
+zeval([quote, Sexp], Sexp).
+
 repl :-
     write('zlang> '),
     flush_output,
     read_line_to_string(user_input, Line),
-    (   Line == end_of_file
-    ->  nl, writeln('Ta ta!')
-    ;   (   zread(Line, Sexp)
-        ->  writeln(Sexp)
-        ;   writeln('Gibberish.')
-        ),
-        repl
-    ).
+    (Line == end_of_file -> nl, writeln('Ta ta!');
+     (zread(Line, Sexp) -> writeln(Sexp); 
+      writeln('Gibberish.')),
+     repl).
 
 :- initialization(repl, main).
